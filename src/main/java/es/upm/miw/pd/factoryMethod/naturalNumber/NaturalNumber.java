c61b962b5c0973aa0c367d4773b5d72f3ee16bc0
Robin Giles Ribera <robinchogiles@gmail.com>
2014-10-18 22:50:02 +0200

    Completado FactoryMethod NAturalNumber

--- src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/NaturalNumber.java
+++ src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/NaturalNumber.java
@@ -2,17 +2,31 @@ package es.upm.miw.pd.factoryMethod.naturalNumber;
 
 public class NaturalNumber {
 	
-	private int number;
+	private int value;
 	
-	public NaturalNumber(int number) {
-		this.setNumber(number);
+	public NaturalNumber(int value) {
+		this.setValue(value);
 	}
 
-	public int getNumber() {
-		return number;
-	}
+    public int getValue() {
+        return value;
+    }
 
-	public void setNumber(int number) {
-		this.number = number;
-	}
+    public void setValue(int value) {
+        assert value >= 0;
+        this.value = value;
+    }
+
+    public void add(int value) {
+        this.setValue(this.value + value);
+    }
+
+    public String getTextValue(String[] textValue) {
+        if (this.value < textValue.length) {
+            return textValue[this.value];
+        } else {
+            return "???";
+        }
+    }
+
 }
--- src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/NaturalNumberEn.java
+++ src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/NaturalNumberEn.java
@@ -1,5 +1,15 @@
 package es.upm.miw.pd.factoryMethod.naturalNumber;
 
-public class NaturalNumberEn {
+public class NaturalNumberEn extends NaturalNumber {
+	
+	private static final String[] textValue = {"zero", "one", "two", "three", "four", "five"};
 
+	public NaturalNumberEn(int value) {
+		super(value);
+	}
+
+	public String getTextValue() {
+		return super.getTextValue(textValue);
+	}
+	
 }
